d3014badb5c2695419e2a5de7b2e42b355d41603
kfkitsune <kfkitsune@users.noreply.github.com>
2016-10-27 12:31:33 -0400

    [WIP] Add additional tests

--- weasyl/test/searchtag/test_associate.py
+++ weasyl/test/searchtag/test_associate.py
@@ -6,9 +6,9 @@ from weasyl import profile, searchtag
 from weasyl.error import WeasylError
 from weasyl.test import db_utils
 
-# Tag set for testing
-tags = ['apple', 'orange']
-
+# Tag sets for testing
+tags = searchtag.parse_tags("omega_ruby, alpha_sapphire, diamond, pearl")
+tags_two = searchtag.parse_tags("omega_ruby, alpha_sapphire, diamond")
 
 @pytest.mark.usefixtures('db')
 def test_TargetRecordMissing_WeasylError_if_item_record_missing_or_invalid():
@@ -72,6 +72,89 @@ def test_contentOwnerIgnoredYou_WeasylError_if_user_ignored_by_item_owner():
     with pytest.raises(WeasylError) as err:
         searchtag.associate(userid_tag_adder, tags, journalid=journalid)
     assert err.value.value == "contentOwnerIgnoredYou"
+
+
+@pytest.mark.usefixtures('db')
+def test_adding_tags_when_no_tags_previously_existed():
+    userid_owner = db_utils.create_user()
+    userid_tag_adder = db_utils.create_user()
+    journalid = db_utils.create_journal(userid_owner)
+    charid = db_utils.create_character(userid_owner)
+    submitid = db_utils.create_submission(userid_owner)
+
+    searchtag.associate(userid_tag_adder, tags, submitid=submitid)
+    searchtag.associate(userid_tag_adder, tags, charid=charid)
+    searchtag.associate(userid_tag_adder, tags, journalid=journalid)
+
+    submitid_tags = searchtag.select(submitid=submitid)
+    for tag in tags:
+        assert tag in submitid_tags
+
+    charid_tags = searchtag.select(charid=charid)
+    for tag in tags:
+        assert tag in charid_tags
+
+    journalid_tags = searchtag.select(journalid=journalid)
+    for tag in tags:
+        assert tag in journalid_tags
+
+
+@pytest.mark.usefixtures('db')
+def test_removing_tags():
+    userid_owner = db_utils.create_user()
+    userid_tag_adder = db_utils.create_user()
+    journalid = db_utils.create_journal(userid_owner)
+    charid = db_utils.create_character(userid_owner)
+    submitid = db_utils.create_submission(userid_owner)
+    searchtag.associate(userid_tag_adder, tags, submitid=submitid)
+    searchtag.associate(userid_tag_adder, tags, charid=charid)
+    searchtag.associate(userid_tag_adder, tags, journalid=journalid)
+    
+    # Remove the 'pearl' tag
+    searchtag.associate(userid_tag_adder, tags_two, submitid=submitid)
+    searchtag.associate(userid_tag_adder, tags_two, charid=charid)
+    searchtag.associate(userid_tag_adder, tags_two, journalid=journalid)
+    
+    submitid_tags = searchtag.select(submitid=submitid)
+    for tag in tags_two:
+        assert tag in submitid_tags
+
+    charid_tags = searchtag.select(charid=charid)
+    for tag in tags_two:
+        assert tag in charid_tags
+
+    journalid_tags = searchtag.select(journalid=journalid)
+    for tag in tags_two:
+        assert tag in journalid_tags
+
+
+@pytest.mark.usefixtures('db')
+def test_clearing_all_tags():
+    userid_owner = db_utils.create_user()
+    userid_tag_adder = db_utils.create_user()
+    journalid = db_utils.create_journal(userid_owner)
+    charid = db_utils.create_character(userid_owner)
+    submitid = db_utils.create_submission(userid_owner)
+
+    searchtag.associate(userid_tag_adder, {}, submitid=submitid)
+    searchtag.associate(userid_tag_adder, {}, charid=charid)
+    searchtag.associate(userid_tag_adder, {}, journalid=journalid)
+
+    submitid_tags = searchtag.select(submitid=submitid)
+    assert submitid_tags == {}
+    charid_tags = searchtag.select(charid=charid)
+    assert charid_tags == {}
+    journalid_tags = searchtag.select(journalid=journalid)
+    assert journalid_tags == {}
+
+
+
+
+
+
+
+
+
 
 
 
