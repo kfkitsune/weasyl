3c12435490e19841e60cb9a54f0ef9a8362b35fb
kfkitsune <kfkitsune@users.noreply.github.com>
2017-01-19 15:47:52 -0500

    Test storing password auth information on the session
    If the user's login record has the twofa_secret field set, record on the user's session that password authentication passed; we need to store this information here because if we don't have a server-side mechanism to track that PW-auth passed successfully, PW-auth might be bypassable (AKA, don't trust anything the user provides, except a cookie, and even then, trust it for a short period of time; validate timestamp for like... 300 or 600 seconds for a timeout).

--- weasyl/login.py
+++ weasyl/login.py
@@ -57,14 +57,14 @@ def authenticate_bcrypt(username, password, session=True):
 
     # Select the authentication data necessary to check that the the user-entered
     # credentials are valid
-    query = d.execute("SELECT ab.userid, ab.hashsum, lo.settings FROM authbcrypt ab"
+    query = d.execute("SELECT ab.userid, ab.hashsum, lo.settings, lo.twofa_secret FROM authbcrypt ab"
                       " RIGHT JOIN login lo USING (userid)"
                       " WHERE lo.login_name = '%s'", [d.get_sysname(username)], ["single"])
 
     if not query:
         return 0, "invalid"
 
-    USERID, HASHSUM, SETTINGS = query
+    USERID, HASHSUM, SETTINGS, TWOFA = query
     HASHSUM = HASHSUM.encode('utf-8')
 
     d.metric('increment', 'attemptedlogins')
@@ -99,9 +99,20 @@ def authenticate_bcrypt(username, password, session=True):
     # Attempt to create a new session if `session` is True, then log the signin
     # if it succeeded.
     if session:
-        signin(USERID)
-        d.append_to_log('login.success', userid=USERID, ip=d.get_address())
-        d.metric('increment', 'logins')
+        # If the user's record has ``login.twofa_secret`` set (not nulled), return that password authentication succeeded.
+        if TWOFA:
+            # Save to the user's session that we successfully passed password authentication w/time passed
+            sess = d.get_weasyl_session()
+            sess.password_authenticated = True
+            sess.password_authenticated_userid = USERID
+            sess.password_authenticated_time = arrow.now().timestamp
+            sess.save = True
+            # Then return back that we need the 2FA authentication
+            return USERID, "2fa"
+        else:
+            signin(USERID)
+            d.append_to_log('login.success', userid=USERID, ip=d.get_address())
+            d.metric('increment', 'logins')
 
     status = None
     if not unicode_success:
