c2ed581e773386fd73a0438711442131eb350ac0
Kyra <kfkitsune@users.noreply.github.com>
2017-03-28 20:14:19 -0400

    Back-out re-implemented/duplicated code
    Opt for weasyl.define.get_int()

--- libweasyl/libweasyl/security.py
+++ libweasyl/libweasyl/security.py
@@ -21,21 +21,3 @@ def generate_key(size):
         An ASCII printable :term:`native string` of length *size*.
     """
     return "".join(secure_random.choice(key_characters) for i in range(size))
-
-
-def input_validation_integer(int_to_test):
-    """
-    Validate a purported integer (Python `int`) before using it, returning integer
-    zero (0) if `int_to_test` cannot be made into an int.
-
-    Parameters:
-        int_to_test - A claimed integer.
-
-    Returns:
-        If `int_to_test` can be made into a Python Integer object, int(int_to_test);
-        otherwise, if the creation of the int() object fails with a ValueError, integer zero (0).
-    """
-    try:
-        return int(int_to_test)
-    except ValueError:
-        return 0
